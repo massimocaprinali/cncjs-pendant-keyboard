e116d5d25f74c245b6bce343339e5fbd9c3da926
manzyinter <massimo.caprinali@gmail.com>
2019-06-11 17:16:09 +0200

    Add support to Marlin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function(options, callback) {
     options.baudrate = get(options, 'baudrate', 115200);
     options.socketAddress = get(options, 'socketAddress', 'localhost');
     options.socketPort = get(options, 'socketPort', 8000);
-    options.controllerType = get(options, 'controllerType', 'Grbl');
+    options.controllerType = get(options, 'controllerType', 'Marlin');
     options.accessTokenLifetime = get(options, 'accessTokenLifetime', '30d');
 
     if (!options.secret) {
