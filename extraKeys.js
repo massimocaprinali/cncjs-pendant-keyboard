620c423fe056e1ee39c3b16675c291b21b4de62a
MASSIMO CAPRINALI <massimo.caprinali@it.ibm.com>
2020-11-08 22:55:00 +0100

    add external constants

---
+++ extraKeys.js
@@ -0,0 +1,16 @@
+// extraKeys.js
+
+module.exports = {
+    HOME: 148,
+    MUTE: 226,
+    MEDIA_PLAY: 131,
+    VOLUME_DOWN: 234,
+    VOLUME_UP: 233,
+    PLAY_PAUSE: 205,
+    PREVIOUS: 182,
+    NEXT: 181,
+    SEARCH: 33,
+    INTERNET: 35,
+    MOUSE_LEFT: 1,
+    MOUSE_RIGHT: 2
+}
